c1ba3b35f81503e28194f9ab8456b3e5c1704f84
Karen Payne <Karen.1.Payne@oregon.gov>
2019-11-07 07:38:13 -0800

    mocking

--- WpfAppExample1/Classes/Quarter.cs
+++ WpfAppExample1/Classes/Quarter.cs
@@ -71,5 +71,6 @@ namespace WpfAppExample1.Classes
     public class TaxRateInfo
     {
         public int Year { get; set; } = DateTime.Now.Year -2;
+        public int APR { get; set; } = 9;
     }
 }
