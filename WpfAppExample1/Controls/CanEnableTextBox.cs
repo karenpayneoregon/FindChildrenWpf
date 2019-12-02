e1944e3c518c2563400c584d3b515e3ddbcea7b6
Karen Payne <paynekaren@comcast.net>
2019-12-02 13:57:27 -0800

    Work for upcoming article

--- WpfAppExample1/Controls/CanEnableTextBox.cs
+++ WpfAppExample1/Controls/CanEnableTextBox.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace WpfAppExample1
+namespace WpfAppExample1.Controls
 {
     public class CanEnableTextBox : TextBox
     {
+
         static CanEnableTextBox()
         {
             IsEnabledProperty.OverrideMetadata(typeof(CanEnableTextBox),
---
+++ WpfAppExample1/SelectiveReadOnlyWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppExample1
+{
+    /// <summary>
+    /// Interaction logic for SelectiveReadOnlyWindow.xaml
+    /// </summary>
+    public partial class SelectiveReadOnlyWindow : Window
+    {
+        public SelectiveReadOnlyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InviteCodeButton_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var userText = ((TextBox) sender).Text;
+            if (int.TryParse(userText, out var code))
+            {
+                if (code == 123)
+                {
+                    GroupGrid.IsEnabled = true;
+                    CodeImage.Visibility = Visibility.Visible;
+                }
+            }
+        }
+    }
+}
