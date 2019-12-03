998eee55fa15cda2807165a34671dca5acc9b81e
Karen Payne <paynekaren@comcast.net>
2019-12-03 09:18:26 -0800

    Made code TextBox int only

--- WpfAppExample1/SelectiveReadOnlyWindow.xaml.cs
+++ WpfAppExample1/SelectiveReadOnlyWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,6 +36,16 @@ namespace WpfAppExample1
                     //CodeImage.Visibility = Visibility.Visible;
                 }
             }
+        }
+        /// <summary>
+        /// Ensures only numbers for the code verification TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
