37a9fa038bc77d8a3398c48c7b7980125501a570
Karen Payne <paynekaren@comcast.net>
2019-12-18 10:53:47 -0800

    Styling ListBox

--- WpfAppExample1/Classes/TaskItem.cs
+++ WpfAppExample1/Classes/TaskItem.cs
@@ -28,6 +28,7 @@ namespace WpfAppExample1.Classes
             {
                 new TaskItem() {Priority = 2, TaskType = TaskType.Work, TaskName = "Unit test data operations", Description = "Delegate to junior developer"},
                 new TaskItem() {Priority = 1, TaskType = TaskType.Work, TaskName = "Prototype dashboard", Description = "Put together dashboard prototype"},
+                new TaskItem() {Priority = 1, TaskType = TaskType.Home, TaskName = "Cook dinner", Description = "Ah, get a pizza"},
                 new TaskItem() {Priority = 3, TaskType = TaskType.Work, TaskName = "Single signon discussion", Description = "Discuss options"}
             };
         }
